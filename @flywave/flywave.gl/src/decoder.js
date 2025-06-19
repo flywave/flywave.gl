9f69cf26a9ceb3fa8d6ed437048b6ab8f9763326
w12594822 <w12594822@126.com>
2025-06-19 13:07:06 +0800

    Signed-off-by: w12594822 <w12594822@126.com>

--- @flywave/flywave.gl/src/decoder.js
+++ @flywave/flywave.gl/src/decoder.js
@@ -1,19 +1,20 @@
-import "./util/import_threejs";
+let three = require("./util/three.b.js");
+self.THREE = three;
 
-import QuantizedMeshTileDecoderService from "./terrain-source/tin-terrain/tile-decoder-worker";
-import CsgStratumDecoderService from "./terrain-source/stratum/csg-worker";
-import DemTileDecoderService from "./terrain-source/height-map/dem/dem-decoder-worker";
-import {
-  GeoJsonTilerService,
-  VectorTileDecoderService,
-} from "@flywave/flywave-vectortile-datasource/index-worker";
+let QuantizedMeshTileDecoderService = require("./terrain-source/tin-terrain/tile-decoder-worker");
+let CsgStratumDecoderService = require("./terrain-source/stratum/csg-worker");
+let DemTileDecoderService = require("./terrain-source/height-map/dem/dem-decoder-worker");
+let {
+    GeoJsonTilerService,
+    VectorTileDecoderService
+} = require("@flywave/flywave-vectortile-datasource/index-worker");
 // import ObjectDecoderService from "./objects/decoder/decoder";
 
 VectorTileDecoderService.start();
 GeoJsonTilerService.start();
-QuantizedMeshTileDecoderService.start();
-DemTileDecoderService.start();
-CsgStratumDecoderService.start();
+QuantizedMeshTileDecoderService.default.start();
+DemTileDecoderService.default.start();
+CsgStratumDecoderService.default.start();
 // ObjectDecoderService.start();
 
 //Following code is only needed for datasource_custom example.
--- @flywave/flywave.gl/src/util/import_threejs.js
+++ @flywave/flywave.gl/src/util/import_threejs.js
@@ -1,2 +1,1 @@
-
-importScripts("three.js");
+require('./three.b.js')
