ca9edb9a98c376b3553544f525c76b0f41c42afd
w12594822 <w12594822@126.com>
2025-12-10 18:26:50 +0800

    Update lerna configuration and publish script

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -4,5 +4,7 @@
 # Simple script that bundles the publishing of packages to be run from CI
 #
 
+# Temporarily allow adding pnpm-lock.yaml during lerna publish
 echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
-pnpm publish -r --tag alpha
+git config core.safecrlf false
+lerna publish --no-private
